e37cc86439d6bde511664553848dd8bfd6e0ea99
Dharmik Patel <dharmikisback@gmail.com>
2017-07-11 08:33:34 +0530

    [Xamarin] [iOS] [Android] : Sample Lottie Animation

---
+++ demo/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Com.Airbnb.Lottie;
+
+namespace demo
+{
+	[Activity (Label = "demo", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		private LottieAnimationView animationView;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.Main);
+			this.animationView = FindViewById<LottieAnimationView> (Resource.Id.animation_view);
+		}
+		protected override void OnStop ()
+		{
+			base.OnStop ();
+			this.animationView.CancelAnimation ();
+		}
+
+		protected override void OnStart ()
+		{
+			base.OnStart ();
+			this.animationView.Progress = 0f;
+			this.animationView.PlayAnimation ();
+		}
+	}
+}
+
---
+++ demoiOS/ViewController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Airbnb.Lottie;
+using UIKit;
+
+namespace demoiOS
+{
+	public partial class ViewController : UIViewController
+	{
+		private LOTAnimationView lottieLogo;
+		protected ViewController (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			this.lottieLogo = LOTAnimationView.AnimationNamed ("video_cam");
+			this.lottieLogo.ContentMode = UIViewContentMode.ScaleAspectFill;
+			this.lottieLogo.LoopAnimation = true;
+			this.View.AddSubview (this.lottieLogo);
+		}
+
+		private void PlayLottieAnimation ()
+		{
+			this.lottieLogo.AnimationProgress = 0;
+			this.lottieLogo.Play ();
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+			this.lottieLogo.Play ();
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+			this.lottieLogo.Pause ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in us
+		}
+	}
+}
